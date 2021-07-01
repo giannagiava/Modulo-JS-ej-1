a2675c328c3cedea4f8d201c61b7ed1b0b904c0c
Gianna Giavarini <giannagiavarini@outlook.com>
2021-07-01 20:03:34 -0200

    Subiendo ejercicio 1 JS

---
+++ index.js
@@ -0,0 +1,6 @@
+let nombre = prompt("¿Cual es tu nombre?")
+let apellido = prompt("¿Cual es tu apellido?")
+
+let respuesta = (nombre + " " + apellido)
+
+alert ("Hola " + (respuesta) +  ", bienvenida a Ada")
